16f8b933065299f81aeaa3ad0f53bdbd21173031
uuuuuuuk <s22034@gsm.hs.kr>
2023-07-24 10:35:31 +0900

    :tada: 프로젝트 시작

---
+++ src/main/java/Ampersand/GKR/GkrApplication.java
@@ -0,0 +1,13 @@
+package Ampersand.GKR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GkrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GkrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Ampersand/GKR/GkrApplicationTests.java
@@ -0,0 +1,13 @@
+package Ampersand.GKR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GkrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
