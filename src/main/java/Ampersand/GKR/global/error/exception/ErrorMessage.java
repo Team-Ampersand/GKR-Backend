bfd517c5f13b75fe72e5db6c5be1747314d66f5c
uuuuuuuk <s22034@gsm.hs.kr>
2023-07-24 10:59:12 +0900

    :memo: ErrorMessage 작성

---
+++ src/main/java/Ampersand/GKR/global/error/exception/ErrorMessage.java
@@ -0,0 +1,18 @@
+package Ampersand.GKR.global.error.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ErrorMessage {
+
+    private final String message;
+
+    private final int status;
+
+    public ErrorMessage(ErrorCode errorCode){
+        this(errorCode.getMessage(), errorCode.getStatus());
+    }
+}
+
