60d5eebf1aea8dcc6f53811ef89a5e0dd6924346
uuuuuuuk <s22034@gsm.hs.kr>
2023-07-29 21:45:22 +0900

    :memo: UserRepository 작성

---
+++ src/main/java/Ampersand/GKR/domain/user/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package Ampersand.GKR.domain.user.repository;
+
+import Ampersand.GKR.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    Optional<User> findByEmail(String email);
+}
