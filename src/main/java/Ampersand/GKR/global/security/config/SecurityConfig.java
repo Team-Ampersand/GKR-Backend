45ebb63f502a67c46a671edc88c0e95421dc4588
uuuuuuuk <s22034@gsm.hs.kr>
2023-07-29 21:44:06 +0900

    :memo: SecurityConfig 작성

---
+++ src/main/java/Ampersand/GKR/global/security/config/SecurityConfig.java
@@ -0,0 +1,47 @@
+package Ampersand.GKR.global.security.config;
+
+import Ampersand.GKR.global.filter.ExceptionFilter;
+import Ampersand.GKR.global.filter.JwtRequestFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsUtils;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final JwtRequestFilter jwtRequestFilter;
+
+    private final ExceptionFilter exceptionFilter;
+
+    @Bean
+    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .formLogin().disable()
+                .httpBasic().disable()
+                .cors().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
+
+
+
+                .anyRequest().denyAll();
+
+        http
+                .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(exceptionFilter, JwtRequestFilter.class);
+
+        return http.build();
+    }
+}
+
