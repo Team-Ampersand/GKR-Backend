3f075e68ef1515ead2c8211f072d187b75e60bbe
uuuuuuuk <s22034@gsm.hs.kr>
2023-08-05 20:14:26 +0900

    :sparkles: UserEquipmentController 에 모든 기자재 리스트 api 등록

--- src/main/java/Ampersand/GKR/domain/equipment/presentation/UserEquipmentController.java
+++ src/main/java/Ampersand/GKR/domain/equipment/presentation/UserEquipmentController.java
@@ -1,9 +1,22 @@
 package Ampersand.GKR.domain.equipment.presentation;
 
+import Ampersand.GKR.domain.equipment.presentation.dto.response.ListEquipmentResponse;
+import Ampersand.GKR.domain.equipment.service.ListEquipmentService;
 import Ampersand.GKR.global.annotation.RestRequestService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RequiredArgsConstructor
 @RestRequestService("/user/equipment")
 public class UserEquipmentController {
+
+    private final ListEquipmentService listEquipmentService;
+
+    @GetMapping
+    public ResponseEntity<ListEquipmentResponse> list() {
+        var list = listEquipmentService.execute();
+        return new ResponseEntity<>(list, HttpStatus.OK);
+    }
 }
