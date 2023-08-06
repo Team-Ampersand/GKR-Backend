2c543c9681bb1c859aca1ef0ad24267053b4732b
uuuuuuuk <s22034@gsm.hs.kr>
2023-08-06 14:10:48 +0900

    :sparkles: 기자재 상세보기 api 구현

---
+++ src/main/java/Ampersand/GKR/domain/equipment/service/DetailEquipmentService.java
@@ -0,0 +1,29 @@
+package Ampersand.GKR.domain.equipment.service;
+
+import Ampersand.GKR.domain.equipment.entity.Equipment;
+import Ampersand.GKR.domain.equipment.presentation.dto.response.DetailEquipmentResponse;
+import Ampersand.GKR.global.annotation.ReadOnlyService;
+import Ampersand.GKR.global.util.EquipmentUtil;
+import lombok.RequiredArgsConstructor;
+
+@ReadOnlyService
+@RequiredArgsConstructor
+public class DetailEquipmentService {
+
+    private final EquipmentUtil equipmentUtil;
+
+    public DetailEquipmentResponse execute(Long id) {
+
+        Equipment equipment = equipmentUtil.findEquipmentById(id);
+
+        DetailEquipmentResponse detailEquipmentResponse = DetailEquipmentResponse.builder()
+                .id(equipment.getId())
+                .name(equipment.getName())
+                .imageUrl(equipment.getImageUrl())
+                .description(equipment.getDescription())
+                .rentStatus(equipment.getRentStatus())
+                .build();
+
+        return detailEquipmentResponse;
+    }
+}
