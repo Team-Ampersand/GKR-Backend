65afdfb9c436b09f9c3565286b197fd0cbfc8acd
uuuuuuuk <s22034@gsm.hs.kr>
2023-08-06 13:55:22 +0900

    :sparkles: AdminEquipmentController 에 기자재 삭제 api 등록

--- src/main/java/Ampersand/GKR/domain/equipment/presentation/AdminEquipmentController.java
+++ src/main/java/Ampersand/GKR/domain/equipment/presentation/AdminEquipmentController.java
@@ -3,14 +3,13 @@ package Ampersand.GKR.domain.equipment.presentation;
 import Ampersand.GKR.domain.equipment.presentation.dto.request.CreateEquipmentRequest;
 import Ampersand.GKR.domain.equipment.presentation.dto.response.ListEquipmentResponse;
 import Ampersand.GKR.domain.equipment.service.CreateEquipmentService;
+import Ampersand.GKR.domain.equipment.service.DeleteEquipmentService;
 import Ampersand.GKR.domain.equipment.service.ListEquipmentService;
 import Ampersand.GKR.global.annotation.RestRequestService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RequiredArgsConstructor
@@ -20,6 +19,8 @@ public class AdminEquipmentController {
     private final CreateEquipmentService createEquipmentService;
 
     private final ListEquipmentService listEquipmentService;
+
+    private final DeleteEquipmentService deleteEquipmentService;
 
     @PostMapping
     public ResponseEntity<Void> create(@RequestPart(name = "equipment") CreateEquipmentRequest createEquipmentRequest, @RequestPart(name = "file") MultipartFile file) {
@@ -31,5 +32,11 @@ public class AdminEquipmentController {
     public ResponseEntity<ListEquipmentResponse> list() {
         var list = listEquipmentService.execute();
         return new ResponseEntity<>(list, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        deleteEquipmentService.execute(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
