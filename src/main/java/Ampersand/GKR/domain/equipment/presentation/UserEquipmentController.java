828816e9a961f05306c1c2fa726d8d096478578e
uuuuuuuk <s22034@gsm.hs.kr>
2023-08-07 21:44:06 +0900

    :sparkles: UserEquipmentController 에 대여 상태 필터 리스트 api 등록

--- src/main/java/Ampersand/GKR/domain/equipment/presentation/UserEquipmentController.java
+++ src/main/java/Ampersand/GKR/domain/equipment/presentation/UserEquipmentController.java
@@ -1,12 +1,18 @@
 package Ampersand.GKR.domain.equipment.presentation;
 
+import Ampersand.GKR.domain.equipment.enums.RentStatus;
+import Ampersand.GKR.domain.equipment.presentation.dto.response.DetailEquipmentResponse;
 import Ampersand.GKR.domain.equipment.presentation.dto.response.ListEquipmentResponse;
+import Ampersand.GKR.domain.equipment.service.DetailEquipmentService;
 import Ampersand.GKR.domain.equipment.service.ListEquipmentService;
+import Ampersand.GKR.domain.equipment.service.ListNotRentEquipmentService;
 import Ampersand.GKR.global.annotation.RestRequestService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RequiredArgsConstructor
 @RestRequestService("/user/equipment")
@@ -14,9 +20,25 @@ public class UserEquipmentController {
 
     private final ListEquipmentService listEquipmentService;
 
+    private final ListNotRentEquipmentService listNotRentEquipmentService;
+
+    private final DetailEquipmentService detailEquipmentService;
+
     @GetMapping
     public ResponseEntity<ListEquipmentResponse> list() {
         var list = listEquipmentService.execute();
+        return new ResponseEntity<>(list, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<DetailEquipmentResponse> detail(@PathVariable Long id) {
+        DetailEquipmentResponse detailEquipmentResponse = detailEquipmentService.execute(id);
+        return new ResponseEntity<>(detailEquipmentResponse, HttpStatus.OK);
+    }
+
+    @GetMapping("/rent")
+    public ResponseEntity<ListEquipmentResponse> rentStatusList(@RequestParam RentStatus rentStatus) {
+        var list = listNotRentEquipmentService.execute(rentStatus);
         return new ResponseEntity<>(list, HttpStatus.OK);
     }
 }
