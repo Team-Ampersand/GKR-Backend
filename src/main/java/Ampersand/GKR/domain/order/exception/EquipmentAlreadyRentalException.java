05f392ae2c730346c0347b5a17298ddc5d3bcfd3
uuuuuuuk <s22034@gsm.hs.kr>
2023-08-11 21:13:02 +0900

    :memo: EquipmentAlreadyRentalException 작성

---
+++ src/main/java/Ampersand/GKR/domain/order/exception/EquipmentAlreadyRentalException.java
@@ -0,0 +1,13 @@
+package Ampersand.GKR.domain.order.exception;
+
+import Ampersand.GKR.global.error.exception.ErrorCode;
+import Ampersand.GKR.global.error.exception.GkrException;
+import lombok.Getter;
+
+@Getter
+public class EquipmentAlreadyRentalException extends GkrException {
+
+    public EquipmentAlreadyRentalException() {
+        super(ErrorCode.ALREADY_ORDER_EQUIPMENT);
+    }
+}
