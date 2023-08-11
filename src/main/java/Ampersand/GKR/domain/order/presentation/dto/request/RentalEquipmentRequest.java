e09e6cd8ce6a7773e68553b7087b1a347d39d135
uuuuuuuk <s22034@gsm.hs.kr>
2023-08-11 21:12:55 +0900

    :memo: RentalEquipmentRequest 작성

---
+++ src/main/java/Ampersand/GKR/domain/order/presentation/dto/request/RentalEquipmentRequest.java
@@ -0,0 +1,16 @@
+package Ampersand.GKR.domain.order.presentation.dto.request;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class RentalEquipmentRequest {
+
+    private String rentalDate;
+
+    private String reason;
+}
