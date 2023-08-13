b5bc4b6c96a533363f7123f9d808b9a051545666
uuuuuuuk <s22034@gsm.hs.kr>
2023-08-14 02:40:05 +0900

    :sparkles: ApplicationController -> OrderController 로 변경 및 url / endpoint 변경, 신청 상태별 리스트 api 등록

--- src/main/java/Ampersand/GKR/domain/order/presentation/OrderController.java
+++ src/main/java/Ampersand/GKR/domain/order/presentation/OrderController.java
@@ -1,24 +1,33 @@
 package Ampersand.GKR.domain.order.presentation;
 
+import Ampersand.GKR.domain.order.enums.OrderStatus;
 import Ampersand.GKR.domain.order.presentation.dto.request.RentalEquipmentRequest;
+import Ampersand.GKR.domain.order.presentation.dto.response.ListOrderEquipmentResponse;
+import Ampersand.GKR.domain.order.service.ListOrderStatusEquipmentService;
 import Ampersand.GKR.domain.order.service.RentalEquipmentService;
 import Ampersand.GKR.global.annotation.RestRequestService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
-@RestRequestService("/application")
-public class ApplicationController {
+@RestRequestService("/order")
+public class OrderController {
 
     private final RentalEquipmentService rentalEquipmentService;
+
+    private final ListOrderStatusEquipmentService listOrderStatusEquipmentService;
 
     @PostMapping("/rental/{id}")
     public ResponseEntity<Void> rental(@PathVariable Long id, @RequestBody RentalEquipmentRequest rentalEquipmentRequest) {
         rentalEquipmentService.execute(id, rentalEquipmentRequest);
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @GetMapping("/state")
+    public ResponseEntity<ListOrderEquipmentResponse> orderStateList(@RequestParam OrderStatus orderStatus) {
+        var list = listOrderStatusEquipmentService.execute(orderStatus);
+        return new ResponseEntity<>(list, HttpStatus.OK);
     }
 }
