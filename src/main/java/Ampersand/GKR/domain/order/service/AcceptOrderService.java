0693f35c14cce5c769b60d76cf6ead7a78d1d87f
uuuuuuuk <s22034@gsm.hs.kr>
2023-08-14 05:55:30 +0900

    :sparkles: 요청 승인 api 구현

---
+++ src/main/java/Ampersand/GKR/domain/order/service/AcceptOrderService.java
@@ -0,0 +1,40 @@
+package Ampersand.GKR.domain.order.service;
+
+import Ampersand.GKR.domain.equipment.enums.EquipmentStatus;
+import Ampersand.GKR.domain.order.entity.Application;
+import Ampersand.GKR.domain.order.enums.OrderStatus;
+import Ampersand.GKR.domain.order.repository.ApplicationRepository;
+import Ampersand.GKR.domain.user.entity.User;
+import Ampersand.GKR.global.annotation.RollbackService;
+import Ampersand.GKR.global.util.OrderUtil;
+import Ampersand.GKR.global.util.UserUtil;
+import lombok.RequiredArgsConstructor;
+
+@RollbackService
+@RequiredArgsConstructor
+public class AcceptOrderService {
+
+    private final OrderUtil orderUtil;
+
+    private final UserUtil userUtil;
+
+    private final ApplicationRepository applicationRepository;
+
+    public void execute(Long id) {
+
+        User user = userUtil.currentUser();
+
+        Application application = orderUtil.findApplicationById(id);
+
+        application.setOrderStatus(OrderStatus.ACCEPT);
+
+        switch (application.getOrderType()) {
+            case RENTAL :
+                application.getEquipment().setEquipmentStatus(EquipmentStatus.RENTING);
+                return;
+            case RETURN:
+                application.getEquipment().setEquipmentStatus(EquipmentStatus.NOT_RENT);
+                applicationRepository.delete(application);
+        }
+    }
+}
