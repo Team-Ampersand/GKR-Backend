67b40f6a027eb981c06ecfac5a5a2fdf2727bb32
uuuuuuuk <s22034@gsm.hs.kr>
2023-08-28 19:21:37 +0900

    :memo: 임의로 설정하던 일자를 LocalDateTime 으로 설정하도록 변경

--- src/main/java/Ampersand/GKR/domain/order/presentation/dto/request/ExtensionEquipmentRequest.java
+++ src/main/java/Ampersand/GKR/domain/order/presentation/dto/request/ExtensionEquipmentRequest.java
@@ -10,7 +10,5 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ExtensionEquipmentRequest {
 
-    private String rentalDate;
-
     private String reason;
 }
