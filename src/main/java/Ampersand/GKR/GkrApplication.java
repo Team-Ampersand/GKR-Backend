e67a4e6ab2d897dfae9265bd9850dd79758eb219
uuuuuuuk <s22034@gsm.hs.kr>
2023-09-04 18:24:31 +0900

    :memo: GkrApplication 에 @EnableScheduling 어노테이션 작성 및 프로젝트 시간대 설정

--- src/main/java/Ampersand/GKR/GkrApplication.java
+++ src/main/java/Ampersand/GKR/GkrApplication.java
@@ -5,14 +5,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.TimeZone;
 
 @EnableCaching
+@EnableScheduling
 @SpringBootApplication
 @ConfigurationPropertiesScan
 @EnableConfigurationProperties
 public class GkrApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 		SpringApplication.run(GkrApplication.class, args);
 	}
 
