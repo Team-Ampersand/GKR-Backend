92858f0f7cfd7b790a18708bb19cf10de2ae7b95
uuuuuuuk <s22034@gsm.hs.kr>
2023-09-04 18:26:08 +0900

    :memo: ViolationCheckSchedule 작성

---
+++ src/main/java/Ampersand/GKR/domain/violation/schedule/ViolationCheckSchedule.java
@@ -0,0 +1,18 @@
+package Ampersand.GKR.domain.violation.schedule;
+
+import Ampersand.GKR.domain.violation.service.ViolationCheckService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class ViolationCheckSchedule {
+
+    private final ViolationCheckService violationCheckService;
+
+    @Scheduled(cron = "0 0 0")
+    public void violationCheck() {
+        violationCheckService.execute();
+    }
+}
