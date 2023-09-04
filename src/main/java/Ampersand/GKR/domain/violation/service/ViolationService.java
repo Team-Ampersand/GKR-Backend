a8a9fedb23f725d0551f72f7cd397c948e65e4b4
uuuuuuuk <s22034@gsm.hs.kr>
2023-09-04 18:27:49 +0900

    :sparkles: 유저 제재 api 구현

---
+++ src/main/java/Ampersand/GKR/domain/violation/service/ViolationService.java
@@ -0,0 +1,36 @@
+package Ampersand.GKR.domain.violation.service;
+
+import Ampersand.GKR.domain.user.entity.User;
+import Ampersand.GKR.domain.user.exception.UserNotFoundException;
+import Ampersand.GKR.domain.user.repository.UserRepository;
+import Ampersand.GKR.domain.violation.entity.Violation;
+import Ampersand.GKR.domain.violation.presentation.dto.request.ViolationRequest;
+import Ampersand.GKR.domain.violation.repository.ViolationRepository;
+import Ampersand.GKR.global.annotation.RollbackService;
+import lombok.RequiredArgsConstructor;
+
+@RollbackService
+@RequiredArgsConstructor
+public class ViolationService {
+
+    private final UserRepository userRepository;
+
+    private final ViolationRepository violationRepository;
+
+    public void execute(ViolationRequest violationRequest) {
+
+        User user = userRepository.findByEmail(violationRequest.getEmail())
+                .orElseThrow(() -> new UserNotFoundException());
+
+        Violation violation = Violation.builder()
+                .violationReason(violationRequest.getViolationReason())
+                .user(user)
+                .build();
+
+        violationRepository.save(violation);
+
+        violation.setViolationDates();
+
+        user.setRentalRestricted(true);
+    }
+}
