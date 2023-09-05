fa2fdb00753cc11c05c9ed15b01b46c18cbcc78a
uuuuuuuk <s22034@gsm.hs.kr>
2023-09-05 14:26:56 +0900

    :beers: Scheduled 설정 변경

--- src/main/java/Ampersand/GKR/domain/violation/schedule/ViolationCheckSchedule.java
+++ src/main/java/Ampersand/GKR/domain/violation/schedule/ViolationCheckSchedule.java
@@ -11,7 +11,7 @@ public class ViolationCheckSchedule {
 
     private final ViolationCheckService violationCheckService;
 
-    @Scheduled(cron = "0 0 0")
+    @Scheduled(cron = "0 0 0 * * ?")
     public void violationCheck() {
         violationCheckService.execute();
     }
