cb6e84bd63fc2c2a448a3020b9a9ae4b271a5a9b
uuuuuuuk <s22034@gsm.hs.kr>
2023-09-05 14:28:19 +0900

    :sparkles: ViolationController 에 제재 취소 api 등록

--- src/main/java/Ampersand/GKR/domain/violation/presentation/ViolationController.java
+++ src/main/java/Ampersand/GKR/domain/violation/presentation/ViolationController.java
@@ -1,11 +1,14 @@
 package Ampersand.GKR.domain.violation.presentation;
 
+import Ampersand.GKR.domain.violation.presentation.dto.request.ViolationCancelRequest;
 import Ampersand.GKR.domain.violation.presentation.dto.request.ViolationRequest;
+import Ampersand.GKR.domain.violation.service.ViolationCancelService;
 import Ampersand.GKR.domain.violation.service.ViolationService;
 import Ampersand.GKR.global.annotation.RestRequestService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -15,9 +18,17 @@ public class ViolationController {
 
     private final ViolationService violationService;
 
+    private final ViolationCancelService violationCancelService;
+
     @PostMapping
     public ResponseEntity<Void> violation(@RequestBody ViolationRequest violationRequest) {
         violationService.execute(violationRequest);
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @PatchMapping
+    public ResponseEntity<Void> violationCancel(@RequestBody ViolationCancelRequest violationCancelRequest) {
+        violationCancelService.execute(violationCancelRequest);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
