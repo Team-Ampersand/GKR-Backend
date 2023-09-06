c9aa6db64d8cc828bb90b26d2db742352fdd2cd1
uuuuuuuk <s22034@gsm.hs.kr>
2023-09-06 09:39:41 +0900

    :memo: ViolationRepository 에 findByUser() 메서드 추가

--- src/main/java/Ampersand/GKR/domain/violation/repository/ViolationRepository.java
+++ src/main/java/Ampersand/GKR/domain/violation/repository/ViolationRepository.java
@@ -1,8 +1,12 @@
 package Ampersand.GKR.domain.violation.repository;
 
+import Ampersand.GKR.domain.user.entity.User;
 import Ampersand.GKR.domain.violation.entity.Violation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ViolationRepository extends JpaRepository<Violation, Long>, ViolationRepositoryCustom {
 
+    List<Violation> findByUser(User user);
 }
