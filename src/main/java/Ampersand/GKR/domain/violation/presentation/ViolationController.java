e6e3899542c91d9a42d6a9cdec833678a15905d4
uuuuuuuk <s22034@gsm.hs.kr>
2023-09-06 09:41:47 +0900

    :sparkles: ViolationController 에 자신의 제재 내역 리스트 api 등록

--- src/main/java/Ampersand/GKR/domain/violation/presentation/ViolationController.java
+++ src/main/java/Ampersand/GKR/domain/violation/presentation/ViolationController.java
@@ -2,12 +2,15 @@ package Ampersand.GKR.domain.violation.presentation;
 
 import Ampersand.GKR.domain.violation.presentation.dto.request.ViolationCancelRequest;
 import Ampersand.GKR.domain.violation.presentation.dto.request.ViolationRequest;
+import Ampersand.GKR.domain.violation.presentation.dto.response.ListViolationInfoResponse;
+import Ampersand.GKR.domain.violation.service.ListViolationService;
 import Ampersand.GKR.domain.violation.service.ViolationCancelService;
 import Ampersand.GKR.domain.violation.service.ViolationService;
 import Ampersand.GKR.global.annotation.RestRequestService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,6 +23,8 @@ public class ViolationController {
 
     private final ViolationCancelService violationCancelService;
 
+    private final ListViolationService listViolationService;
+
     @PostMapping
     public ResponseEntity<Void> violation(@RequestBody ViolationRequest violationRequest) {
         violationService.execute(violationRequest);
@@ -30,5 +35,11 @@ public class ViolationController {
     public ResponseEntity<Void> violationCancel(@RequestBody ViolationCancelRequest violationCancelRequest) {
         violationCancelService.execute(violationCancelRequest);
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<ListViolationInfoResponse> violationInfo() {
+        var list = listViolationService.execute();
+        return new ResponseEntity<>(list, HttpStatus.OK);
     }
 }
