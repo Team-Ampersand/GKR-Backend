c65de8f10e40262372b9e30ff67566577ec9f474
uuuuuuuk <s22034@gsm.hs.kr>
2023-10-19 18:06:08 +0900

    :sparkles: NoticeController 에 공지글 삭제 api 등록

--- src/main/java/Ampersand/GKR/domain/notice/presentation/NoticeController.java
+++ src/main/java/Ampersand/GKR/domain/notice/presentation/NoticeController.java
@@ -2,10 +2,13 @@ package Ampersand.GKR.domain.notice.presentation;
 
 import Ampersand.GKR.domain.equipment.presentation.dto.request.CreateEquipmentRequest;
 import Ampersand.GKR.domain.equipment.service.CreateEquipmentService;
+import Ampersand.GKR.domain.equipment.service.DeleteEquipmentService;
 import Ampersand.GKR.global.annotation.RestRequestService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
@@ -16,9 +19,17 @@ public class NoticeController {
 
     private final CreateEquipmentService createEquipmentService;
 
+    private final DeleteEquipmentService deleteEquipmentService;
+
     @PostMapping
     public ResponseEntity<Void> create(@RequestPart(name = "notice") CreateEquipmentRequest createEquipmentRequest, @RequestPart(name = "file") MultipartFile file) {
         createEquipmentService.execute(createEquipmentRequest, file);
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        deleteEquipmentService.execute(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
