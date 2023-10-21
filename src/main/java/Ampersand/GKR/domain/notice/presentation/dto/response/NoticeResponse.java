123bffe4d6c043054c5c67b7830dc29f3ce4ddd2
uuuuuuuk <s22034@gsm.hs.kr>
2023-10-21 22:21:28 +0900

    :memo: NoticeResponse 작성

---
+++ src/main/java/Ampersand/GKR/domain/notice/presentation/dto/response/NoticeResponse.java
@@ -0,0 +1,35 @@
+package Ampersand.GKR.domain.notice.presentation.dto.response;
+
+import Ampersand.GKR.domain.notice.entity.Notice;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class NoticeResponse {
+
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+    private String imageUrl;
+
+    private LocalDateTime createNoticeDate;
+
+    public static NoticeResponse toResponse(Notice notice) {
+
+        return NoticeResponse.builder()
+                .id(notice.getId())
+                .title(notice.getTitle())
+                .content(notice.getContent())
+                .imageUrl(notice.getImageUrl())
+                .createNoticeDate(notice.getCreateNoticeDate())
+                .build();
+    }
+}
