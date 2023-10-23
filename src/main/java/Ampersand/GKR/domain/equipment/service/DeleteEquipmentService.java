b751f37ab41197b26c36637595d646cbe05dd9a5
uuuuuuuk <s22034@gsm.hs.kr>
2023-10-23 15:52:26 +0900

    :sparkles: DeleteEquipmentService 에 파일 삭제 및 관련 신청 삭제 로직 추가

--- src/main/java/Ampersand/GKR/domain/equipment/service/DeleteEquipmentService.java
+++ src/main/java/Ampersand/GKR/domain/equipment/service/DeleteEquipmentService.java
@@ -2,6 +2,8 @@ package Ampersand.GKR.domain.equipment.service;
 
 import Ampersand.GKR.domain.equipment.entity.Equipment;
 import Ampersand.GKR.domain.equipment.repository.EquipmentRepository;
+import Ampersand.GKR.domain.file.service.FileUploadService;
+import Ampersand.GKR.domain.order.repository.ApplicationRepository;
 import Ampersand.GKR.domain.user.entity.User;
 import Ampersand.GKR.global.annotation.RollbackService;
 import Ampersand.GKR.global.util.EquipmentUtil;
@@ -14,6 +16,10 @@ public class DeleteEquipmentService {
 
     private final EquipmentRepository equipmentRepository;
 
+    private final ApplicationRepository applicationRepository;
+
+    private final FileUploadService fileUploadService;
+
     private final UserUtil userUtil;
 
     private final EquipmentUtil equipmentUtil;
@@ -24,6 +30,10 @@ public class DeleteEquipmentService {
 
         Equipment equipment = equipmentUtil.findEquipmentById(id);
 
+        if (equipment.getImageUrl() != null) {
+            fileUploadService.deleteFile(equipment.getImageUrl());
+        }
+        applicationRepository.deleteAllByEquipment(equipment);
         equipmentRepository.delete(equipment);
     }
 }
