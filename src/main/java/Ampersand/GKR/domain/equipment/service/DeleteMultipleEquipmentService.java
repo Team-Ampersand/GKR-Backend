a64845cb3ad44886a768cd8ff329ea547beb4988
uuuuuuuk <s22034@gsm.hs.kr>
2023-10-23 15:53:07 +0900

    :sparkles: 기자재 일괄 삭제 api 구현

---
+++ src/main/java/Ampersand/GKR/domain/equipment/service/DeleteMultipleEquipmentService.java
@@ -0,0 +1,41 @@
+package Ampersand.GKR.domain.equipment.service;
+
+import Ampersand.GKR.domain.equipment.entity.Equipment;
+import Ampersand.GKR.domain.equipment.presentation.dto.request.DeleteMultipleEquipmentRequest;
+import Ampersand.GKR.domain.equipment.repository.EquipmentRepository;
+import Ampersand.GKR.domain.file.service.FileUploadService;
+import Ampersand.GKR.domain.order.repository.ApplicationRepository;
+import Ampersand.GKR.global.annotation.RollbackService;
+import Ampersand.GKR.global.util.EquipmentUtil;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@RollbackService
+@RequiredArgsConstructor
+public class DeleteMultipleEquipmentService {
+
+    private final EquipmentRepository equipmentRepository;
+
+    private final ApplicationRepository applicationRepository;
+
+    private final FileUploadService fileUploadService;
+
+    private final EquipmentUtil equipmentUtil;
+
+    public void execute(DeleteMultipleEquipmentRequest deleteMultipleEquipmentRequest) {
+
+        List<Long> equipmentIdList = deleteMultipleEquipmentRequest.getEquipmentIdList();
+
+        for (Long equipmentId : equipmentIdList) {
+
+            Equipment equipment = equipmentUtil.findEquipmentById(equipmentId);
+
+            if (equipment.getImageUrl() != null) {
+                fileUploadService.deleteFile(equipment.getImageUrl());
+            }
+            applicationRepository.deleteAllByEquipment(equipment);
+            equipmentRepository.delete(equipment);
+        }
+    }
+}
