f1b3f9a8b65cc4c366d54d9af8ca275c69a81021
uuuuuuuk <s22034@gsm.hs.kr>
2024-05-09 14:46:17 +0900

    :memo: RedisConfig 에 redisTemplateManager 함수 구현

--- src/main/java/Ampersand/GKR/global/redis/RedisConfig.java
+++ src/main/java/Ampersand/GKR/global/redis/RedisConfig.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 @RequiredArgsConstructor
@@ -18,5 +20,14 @@ public class RedisConfig {
     @ConditionalOnMissingBean(RedisConnectionFactory.class)
     RedisConnectionFactory redisConnectionFactory() {
         return new LettuceConnectionFactory(redisProperties.getHost(), redisProperties.getPort());
+    }
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplateManager() {
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        return redisTemplate;
     }
 }
