e4aea214265e63c7598a84ae4715f0e44835ffce
uuuuuuuk <s22034@gsm.hs.kr>
2024-05-09 14:45:15 +0900

    :memo: EnableCachingConfig 작성

---
+++ src/main/java/Ampersand/GKR/global/config/EnableCachingConfig.java
@@ -0,0 +1,9 @@
+package Ampersand.GKR.global.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class EnableCachingConfig {
+}
