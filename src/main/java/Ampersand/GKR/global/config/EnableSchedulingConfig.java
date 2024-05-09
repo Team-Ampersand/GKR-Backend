859b782abfc5729687adc86978aa12bb7deb5344
uuuuuuuk <s22034@gsm.hs.kr>
2024-05-09 14:45:26 +0900

    :memo: EnableSchedulingConfig 작성

---
+++ src/main/java/Ampersand/GKR/global/config/EnableSchedulingConfig.java
@@ -0,0 +1,9 @@
+package Ampersand.GKR.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class EnableSchedulingConfig {
+}
