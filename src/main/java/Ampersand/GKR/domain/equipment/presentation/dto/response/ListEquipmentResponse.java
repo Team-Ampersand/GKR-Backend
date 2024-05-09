3bf332a02adb9f7d117b462c2fc1d9f91e3fc019
uuuuuuuk <s22034@gsm.hs.kr>
2024-05-09 14:46:48 +0900

    :memo: ListEquipmentResponse 에 NoArgsConstructor 어노테이션을 통한 기본 생성자 추가

--- src/main/java/Ampersand/GKR/domain/equipment/presentation/dto/response/ListEquipmentResponse.java
+++ src/main/java/Ampersand/GKR/domain/equipment/presentation/dto/response/ListEquipmentResponse.java
@@ -3,12 +3,14 @@ package Ampersand.GKR.domain.equipment.presentation.dto.response;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Getter
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class ListEquipmentResponse {
 
     private List<EquipmentResponse> equipmentList;
